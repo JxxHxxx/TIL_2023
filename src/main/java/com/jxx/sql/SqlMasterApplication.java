f0fdc0a4967dc806faad3b5cbc701ba08f030add
JxxHxxx <leesin5498@naver.com>
2023-08-06 21:22:28 +0900

    project setting

---
+++ src/main/java/com/jxx/sql/SqlMasterApplication.java
@@ -0,0 +1,13 @@
+package com.jxx.sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlMasterApplication.class, args);
+    }
+
+}
